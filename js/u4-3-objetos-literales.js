867c9f7ad4651395902b2283834b05a0465a198c
lucasc84 <curtolbt@gmail.com>
2025-04-20 23:09:36 -0300

    añadi clase 4 punto r parcial

---
+++ js/u4-3-objetos-literales.js
@@ -0,0 +1,12 @@
+const persona = {
+    nombre: "Juan",
+    edad: 30,
+    ciudad: "Buenos Aires"
+};
+
+
+console.log(persona.nombre); // Salida: Juan
+console.log(persona.edad); // Salida: 30    
+console.log(persona.ciudad); // Salida: Buenos Aires
+console.log(persona); // Salida: { nombre: 'Juan', edad: 30, ciudad: 'Buenos Aires' }
+
