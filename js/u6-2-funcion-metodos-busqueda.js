2b3fe0ee9f58b83039d7ff9532e90e5b2f44efbf
lucasc84 <curtolbt@gmail.com>
2025-05-06 01:08:18 -0300

    añadi unidad 6 punto 2 final

--- js/u6-2-funcion-metodos-busqueda.js
+++ js/u6-2-funcion-metodos-busqueda.js
@@ -23,3 +23,41 @@ numeros2.forEach(function(numero) {
 });
 
 console.log(duplicados); // [2, 4, 6, 8]
+
+
+// Ejemplo Avanzado
+
+const cursos = [
+    { nombre: 'JavaScript',
+        duracion: 30,
+        precio: 17000,
+     },
+    { nombre: 'Python',
+        duracion: 40,
+        precio: 20000,
+     },
+    { nombre: 'Java',
+        duracion: 50,
+        precio: 25000,
+     },
+    { nombre: 'C++',
+        duracion: 60,
+        precio: 30000,
+     },
+    { nombre: 'JavaScript',
+        duracion: 70,
+        precio: 35000,
+     },
+];
+
+const resultado = cursos.find(curso => curso.nombre === 'JavaScript');
+console.log(resultado); // { nombre: 'JavaScript', duracion: 30, precio: 17000 }
+
+const resultado2 = cursos.find(curso => curso.precio > 20000);
+console.log(resultado2); // { nombre: 'Java', duracion: 50, precio: 25000 }
+
+const noExiste = cursos.find(curso => curso.nombre === 'PHP');
+console.log(noExiste); // undefined
+
+const todosJavaScript = cursos.filter(curso => curso.nombre === 'JavaScript');
+console.log(todosJavaScript); // [{ nombre: 'JavaScript', duracion: 30, precio: 17000 }, { nombre: 'Javascript', duracion: 70, precio: 35000 }]
